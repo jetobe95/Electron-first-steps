eb041da560a383c698ea59a1d6afd1b66de1de72
fabirt <fandig27@gmail.com>
2018-08-14 21:05:56 -0500

    Update

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ const url = require('url');
 
 var mainWindow;
 app.on('ready',function(){
-	mainWindow = new BrowserWindow({width: 975, height: 610,backgroundColor: '#d9d9d9'});
+	mainWindow = new BrowserWindow({
+        width: 975, 
+        height: 610,
+        backgroundColor: '#d9d9d9',
+        resizable: false
+    });
 	//mainWindow.loadURL('https://github.com');
 	 mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
